b67593638af7b8828e2280139cd8de8e28d8c924
jdacode <49216638+jdacode@users.noreply.github.com>
2025-08-21 12:43:46 +0930

    Changed variables

--- install/comfyui-install.sh
+++ install/comfyui-install.sh
@@ -20,19 +20,19 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 application_name="${APPLICATION}"
-app_path="/opt/${application_name}"
-python_path="${app_path}/venv/bin/python"
-skip_user_config="${skip_user_config:-N}"
-python_version_uv="${python_version_uv:-3.12}"
-port_arg="${port_arg:-8188}"
+comfyui_path="/opt/${application_name}"
+comfyui_python_path="${comfyui_path}/venv/bin/python"
+comfyui_port_arg="8188"
+comfyui_skip_user_config="${comfyui_skip_user_config:-N}"
+comfyui_python_version_uv="${comfyui_python_version_uv:-3.12}"
 comfyui_python_args="${comfyui_python_args:---cpu}"
-gpu_type="${gpu_type:-none}"
-comfyui_python_index_url_nvidia="${comfyui_python_index_url_nvidia:-https://download.pytorch.org/whl/cu128}"
-comfyui_python_index_url_amd="${comfyui_python_index_url_amd:-https://download.pytorch.org/whl/rocm6.3}"
-comfyui_python_index_url_intel="${comfyui_python_index_url_intel:-https://download.pytorch.org/whl/xpu}"
+comfyui_gpu_type="${comfyui_gpu_type:-none}"
+comfyui_index_url_nvidia="${comfyui_index_url_nvidia:-https://download.pytorch.org/whl/cu129}"
+comfyui_index_url_amd="${comfyui_index_url_amd:-https://download.pytorch.org/whl/rocm6.4}"
+comfyui_index_url_intel="${comfyui_index_url_intel:-https://download.pytorch.org/whl/xpu}"
 comfyui_manager_enabled="${comfyui_manager_enabled:-Y}"
 
-if ! [[ "${skip_user_config,,}" =~ ^(y|yes)$ ]]; then
+if ! [[ "${comfyui_skip_user_config,,}" =~ ^(y|yes)$ ]]; then
   echo
   echo
   echo "${TAB3}${TAB3}Choose the GPU type for ComfyUI:"
@@ -45,11 +45,11 @@ if ! [[ "${skip_user_config,,}" =~ ^(y|yes)$ ]]; then
   read -rp "${TAB3}${TAB3}${TAB3}Enter your choice [1-4] (default: 1): " gpu_choice
   gpu_choice=${gpu_choice:-1}
   case "$gpu_choice" in
-    1) gpu_type="none";;
-    2) gpu_type="nvidia";;
-    3) gpu_type="amd";;
-    4) gpu_type="intel";;
-    *) gpu_type="none"; echo "${TAB3}${TAB3}${TAB3}${TAB3}Invalid choice. Defaulting to ${gpu_type}." ;;
+    1) comfyui_gpu_type="none";;
+    2) comfyui_gpu_type="nvidia";;
+    3) comfyui_gpu_type="amd";;
+    4) comfyui_gpu_type="intel";;
+    *) comfyui_gpu_type="none"; echo "${TAB3}${TAB3}${TAB3}${TAB3}Invalid choice. Defaulting to ${comfyui_gpu_type}." ;;
   esac
 
   echo
@@ -58,10 +58,10 @@ if ! [[ "${skip_user_config,,}" =~ ^(y|yes)$ ]]; then
   echo "${TAB3}${TAB3}Enter additional ComfyUI python args: "
   echo "${TAB3}${TAB3}------------------------------------"
   echo
-  echo "${TAB3}${TAB3}Current ExecStart command: main.py --listen --port ${port_arg} ${comfyui_python_args}"
-  echo "${TAB3}${TAB3}This only modifies the part after the arrow: main.py --listen --port ${port_arg}  ===>  ${comfyui_python_args}"
+  echo "${TAB3}${TAB3}Current ExecStart command: main.py --listen --port ${comfyui_port_arg} ${comfyui_python_args}"
+  echo "${TAB3}${TAB3}This only modifies the part after the arrow: main.py --listen --port ${comfyui_port_arg}  ===>  ${comfyui_python_args}"
   echo
-  read -re -i "${comfyui_python_args}" -p "${TAB3}${TAB3}${TAB3}main.py --listen --port ${port_arg}  ===>  " comfyui_python_args
+  read -re -i "${comfyui_python_args}" -p "${TAB3}${TAB3}${TAB3}main.py --listen --port ${comfyui_port_arg}  ===>  " comfyui_python_args
 
   echo
   echo
@@ -73,62 +73,61 @@ if ! [[ "${skip_user_config,,}" =~ ^(y|yes)$ ]]; then
 fi
 
 echo -e "${CM}${BOLD}${DGN}Application name           : ${BGN}${application_name}${CL}"
-echo -e "${CM}${BOLD}${DGN}Application path           : ${BGN}${app_path}${CL}"
-echo -e "${CM}${BOLD}${DGN}Skip User Config           : ${BGN}${skip_user_config}${CL}"
-echo -e "${CM}${BOLD}${DGN}GPU                        : ${BGN}${gpu_type}${CL}"
-echo -e "${CM}${BOLD}${DGN}Port                       : ${BGN}${port_arg}${CL}"
+echo -e "${CM}${BOLD}${DGN}Application path           : ${BGN}${comfyui_path}${CL}"
+echo -e "${CM}${BOLD}${DGN}Skip User Config           : ${BGN}${comfyui_skip_user_config}${CL}"
+echo -e "${CM}${BOLD}${DGN}GPU                        : ${BGN}${comfyui_gpu_type}${CL}"
 echo -e "${CM}${BOLD}${DGN}ComfyUI Manager            : ${BGN}${comfyui_manager_enabled}${CL}"
-echo -e "${CM}${BOLD}${DGN}Python version (uv)        : ${BGN}${python_version_uv}${CL}"
-echo -e "${CM}${BOLD}${DGN}Python path (uv)           : ${BGN}${python_path}${CL}"
+echo -e "${CM}${BOLD}${DGN}Python version (uv)        : ${BGN}${comfyui_python_version_uv}${CL}"
+echo -e "${CM}${BOLD}${DGN}Python path (uv)           : ${BGN}${comfyui_python_path}${CL}"
 echo -e "${CM}${BOLD}${DGN}ComfyUI python args        : ${BGN}${comfyui_python_args}${CL}"
-echo -e "${CM}${BOLD}${DGN}Pip Nvidia index-url       : ${BGN}${comfyui_python_index_url_nvidia}${CL}"
-echo -e "${CM}${BOLD}${DGN}Pip AMD index-url          : ${BGN}${comfyui_python_index_url_amd}${CL}"
-echo -e "${CM}${BOLD}${DGN}Pip Intel index-url        : ${BGN}${comfyui_python_index_url_intel}${CL}"
-echo -e "${CM}${BOLD}${DGN}Preview ExecStart command  : ${BGN}main.py --listen --port ${port_arg} ${comfyui_python_args}${CL}"
+echo -e "${CM}${BOLD}${DGN}Pip Nvidia index-url       : ${BGN}${comfyui_index_url_nvidia}${CL}"
+echo -e "${CM}${BOLD}${DGN}Pip AMD index-url          : ${BGN}${comfyui_index_url_amd}${CL}"
+echo -e "${CM}${BOLD}${DGN}Pip Intel index-url        : ${BGN}${comfyui_index_url_intel}${CL}"
+echo -e "${CM}${BOLD}${DGN}Preview ExecStart command  : ${BGN}main.py --listen --port ${comfyui_port_arg} ${comfyui_python_args}${CL}"
 
 msg_info "Setup uv"
-PYTHON_VERSION="${python_version_uv}" setup_uv
+PYTHON_VERSION="${comfyui_python_version_uv}" setup_uv
 msg_ok "Setup uv"
 
-fetch_and_deploy_gh_release "${application_name}" "comfyanonymous/ComfyUI" "tarball" "latest" "${app_path}"
+fetch_and_deploy_gh_release "${application_name}" "comfyanonymous/ComfyUI" "tarball" "latest" "${comfyui_path}"
 
 msg_info "Python dependencies"
-$STD uv venv "${app_path}/venv"
-if [[ "${gpu_type,,}" == "nvidia" ]]; then
+$STD uv venv "${comfyui_path}/venv"
+if [[ "${comfyui_gpu_type,,}" == "nvidia" ]]; then
   echo "NVIDIA GPU selected"
   $STD uv pip install \
       torch \
       torchvision \
       torchaudio \
-      --extra-index-url "${comfyui_python_index_url_nvidia}" \
-      --python="${python_path}"
-elif [[ "${gpu_type,,}" == "amd" ]]; then
+      --extra-index-url "${comfyui_index_url_nvidia}" \
+      --python="${comfyui_python_path}"
+elif [[ "${comfyui_gpu_type,,}" == "amd" ]]; then
   echo "AMD GPU selected"
   $STD uv pip install \
       torch \
       torchvision \
       torchaudio \
-      --index-url "${comfyui_python_index_url_amd}" \
-      --python="${python_path}"
-elif [[ "${gpu_type,,}" == "intel" ]]; then
+      --index-url "${comfyui_index_url_amd}" \
+      --python="${comfyui_python_path}"
+elif [[ "${comfyui_gpu_type,,}" == "intel" ]]; then
   echo "Intel GPU selected"
   $STD uv pip install \
       torch \
       torchvision \
       torchaudio \
-      --index-url "${comfyui_python_index_url_intel}" \
-      --python="${python_path}"
+      --index-url "${comfyui_index_url_intel}" \
+      --python="${comfyui_python_path}"
 else
   echo "No GPU selected"
 fi
-$STD uv pip install -r "${app_path}/requirements.txt" --python="${python_path}"
+$STD uv pip install -r "${comfyui_path}/requirements.txt" --python="${comfyui_python_path}"
 msg_ok "Python dependencies"
 
 if [[ "${comfyui_manager_enabled,,}" =~ ^(y|yes)$ ]]; then
   msg_info "Install ${application_name} Manager"
-  comfyui_manager_dir="${app_path}/custom_nodes/comfyui-manager"
+  comfyui_manager_dir="${comfyui_path}/custom_nodes/comfyui-manager"
   git clone https://github.com/ltdrdata/ComfyUI-Manager "${comfyui_manager_dir}"
-  $STD uv pip install -r "${comfyui_manager_dir}/requirements.txt" --python="${python_path}"
+  $STD uv pip install -r "${comfyui_manager_dir}/requirements.txt" --python="${comfyui_python_path}"
   msg_ok "Installed ${application_name} Manager"
 else
   msg_error "No installed ${application_name} Manager"
@@ -143,8 +142,8 @@ After=network.target
 [Service]
 Type=simple
 User=root
-WorkingDirectory=${app_path}
-ExecStart=${python_path} ${app_path}/main.py --listen --port ${port_arg} ${comfyui_python_args}
+WorkingDirectory=${comfyui_path}
+ExecStart=${comfyui_python_path} ${comfyui_path}/main.py --listen --port ${comfyui_port_arg} ${comfyui_python_args}
 Restart=on-failure
 
 [Install]
